d3632b0d775d89778a1ba48331f5b20cd1514bc2
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2016-09-05 17:28:12 +0200

    Modified CreateDomDump.f90 and parameters.dat in order to have more flexibility in parametrizing the domain decomposition.

--- f90/CreateDomDump.f90
+++ f90/CreateDomDump.f90
@@ -29,14 +29,24 @@ program CreateDomDump
   character(2000)           :: repository = './'      ! ramses run directory (where all output_xxxxx dirs are).
   integer(kind=4)           :: snapnum = 1            ! ramses output number to use
   ! --- computational domain  
-  character(10)             :: typechar = 'sphere'    ! shape type of domain  // only sphere allowed now. 
-  real(kind=8),dimension(3) :: pos = (/0.5,0.5,0.5/)  ! center of domain [code units]
-  real(kind=8)              :: rvir = 0.3             ! radius of domain [code units]
-  ! --- domain decomposition // Only works with a number of shells now, sharing center wiht sphere above.
-  character(10)             :: decomp_dom_type = 'shell' ! shape type of domain  // only shell allowed now.
-  integer(kind=4)           :: ndomain = 1               ! nb of domains in decomposition
-  real(kind=8),allocatable  :: rin(:)                    ! inner radius of shell [code units], default value 0
-  real(kind=8),allocatable  :: rout(:)                   ! outer radius of shell [code units], default value = min(rvir+0.05,0.5)
+  character(10)             :: comput_dom_type      = 'sphere'         ! shape type of domain  // default is a shpere.
+  real(kind=8),dimension(3) :: comput_dom_pos       = (/0.5,0.5,0.5/)  ! center of domain [code units]
+  real(kind=8)              :: comput_dom_rsp       = 0.3              ! radius of sphere [code units]
+  real(kind=8)              :: comput_dom_size      = 0.3              ! size of cube [code units]
+  real(kind=8)              :: comput_dom_rin       = 0.0              ! inner radius of shell [code units]
+  real(kind=8)              :: comput_dom_rout      = 0.3              ! outer radius of shell [code units]
+  real(kind=8)              :: comput_dom_thickness = 0.1              ! thickness of slab [code units]
+  ! --- domain decomposition // Only works with a number of domains, sharing the same type.
+  character(10)             :: decomp_dom_type = 'sphere'              ! shape type of domain  // default is one sphere.
+  integer(kind=4)           :: decomp_dom_ndomain = 1                  ! nb of domains in decomposition
+  real(kind=8),allocatable  :: decomp_dom_xc(:)                        ! x center of domain(s) [code units], default value 0.5
+  real(kind=8),allocatable  :: decomp_dom_yc(:)                        ! y center of domain(s) [code units], default value 0.5
+  real(kind=8),allocatable  :: decomp_dom_zc(:)                        ! z center of domain(s) [code units], default value 0.5
+  real(kind=8),allocatable  :: decomp_dom_rsp(:)                       ! radius of sphere [code units]
+  real(kind=8),allocatable  :: decomp_dom_rin(:)                       ! inner radius of shell [code units]
+  real(kind=8),allocatable  :: decomp_dom_rout(:)                      ! outer radius of shell [code units]
+  real(kind=8),allocatable  :: decomp_dom_size(:)                      ! size of cube [code units]
+  real(kind=8),allocatable  :: decomp_dom_thickness(:)                 ! thickness of slab [code units]
   ! --- miscelaneous
   logical                   :: verbose = .false.
   ! --------------------------------------------------------------------------
@@ -58,7 +68,20 @@ program CreateDomDump
   ! read user-defined parameters 
   
   ! Define a spherical computational domain. This domain describes the volume in which photons fly.
-  call domain_constructor_from_scratch(domaine_de_calcul,typechar,xc=pos(1),yc=pos(2),zc=pos(3),r=rvir) 
+  select case(comput_dom_type)
+  case('sphere')
+     call domain_constructor_from_scratch(domaine_de_calcul,comput_dom_type, &
+          xc=comput_dom_pos(1),yc=comput_dom_pos(2),zc=comput_dom_pos(3),r=comput_dom_rsp)
+  case('shell')
+     call domain_constructor_from_scratch(domaine_de_calcul,comput_dom_type, &
+          xc=comput_dom_pos(1),yc=comput_dom_pos(2),zc=comput_dom_pos(3),r_inbound=comput_dom_rin,r_outbound=comput_dom_rout)
+  case('cube')
+     call domain_constructor_from_scratch(domaine_de_calcul,comput_dom_type, & 
+          xc=comput_dom_pos(1),yc=comput_dom_pos(2),zc=comput_dom_pos(3),size=comput_dom_size)
+  case('slab')
+     call domain_constructor_from_scratch(domaine_de_calcul,comput_dom_type, &
+          xc=comput_dom_pos(1),yc=comput_dom_pos(2),zc=comput_dom_pos(3),thickness=comput_dom_thickness)
+  end select
 
   
   ! lecture de toutes les feuilles de la simu
@@ -69,11 +92,30 @@ program CreateDomDump
   call gas_from_ramses_leaves(repository,snapnum,nleaftot,nvar,ramses_var, gas_leaves)
 
   ! domain decomposition 
+  if (verbose)then
+     print *
+     print *,'...building domains...'
+  endif
   meshroot = 'domain_'
-  allocate(domain_list(ndomain))
-  allocate(domain_file_list(ndomain),mesh_file_list(ndomain))
-  do i = 1, ndomain
-     call domain_constructor_from_scratch(domain_list(i),decomp_dom_type,xc=pos(1),yc=pos(2),zc=pos(3),r_inbound=rin(i),r_outbound=rout(i))
+  allocate(domain_list(decomp_dom_ndomain))
+  allocate(domain_file_list(decomp_dom_ndomain),mesh_file_list(decomp_dom_ndomain))
+  do i = 1, decomp_dom_ndomain
+     !call domain_constructor_from_scratch(domain_list(i),decomp_dom_type,xc=pos(1),yc=pos(2),zc=pos(3),r_inbound=rin(i),r_outbound=rout(i))
+     !call domain_constructor_from_scratch(domain_list(i),decomp_dom_type,xc=pos(1),yc=pos(2),zc=pos(3),size=rin(i))
+     select case(decomp_dom_type)
+     case('sphere')
+        call domain_constructor_from_scratch(domain_list(i),decomp_dom_type, &
+             xc=decomp_dom_xc(i),yc=decomp_dom_yc(i),zc=decomp_dom_zc(i),r=decomp_dom_rsp(i))
+     case('shell')
+        call domain_constructor_from_scratch(domain_list(i),decomp_dom_type, &
+             xc=decomp_dom_xc(i),yc=decomp_dom_yc(i),zc=decomp_dom_zc(i),r_inbound=decomp_dom_rin(i),r_outbound=decomp_dom_rout(i))
+     case('cube')
+        call domain_constructor_from_scratch(domain_list(i),decomp_dom_type, & 
+             xc=decomp_dom_xc(i),yc=decomp_dom_yc(i),zc=decomp_dom_zc(i),size=decomp_dom_size(i))
+     case('slab')
+        call domain_constructor_from_scratch(domain_list(i),decomp_dom_type, &
+             xc=decomp_dom_xc(i),yc=decomp_dom_yc(i),zc=decomp_dom_zc(i),thickness=decomp_dom_thickness(i))
+     end select
      write(toto,'(i8)') i
      write(toto,'(a)') adjustl(toto)  ! remove leading spaces
      write(domain_file_list(i),'(a,a,a)') trim(meshroot),trim(toto),'.dom'
@@ -87,8 +129,8 @@ program CreateDomDump
   fichier2 = "MCLya_domain_params.dat"
   open(unit=10, file=trim(datadir)//trim(fichier2))
   write(10,*) 'computational_domain_file = ',trim(fichier)
-  write(10,*) 'Ndomain = ',ndomain
-  do i=1,ndomain
+  write(10,*) 'Ndomain = ',decomp_dom_ndomain
+  do i=1,decomp_dom_ndomain
      write(10,*) 'domain_file = ',trim(domain_file_list(i))
      write(10,*) 'mesh_file   = ',trim(mesh_file_list(i))
      fichier = trim(datadir)//trim(domain_file_list(i))
@@ -97,7 +139,7 @@ program CreateDomDump
   close(10)
 
   ! creation des mesh
-  do i = 1,ndomain
+  do i = 1,decomp_dom_ndomain
      call select_in_domain(domain_list(i), nleaftot, x_leaf, ind_sel)
      call select_from_domain(arr_in=x_leaf,     ind_sel=ind_sel, arr_out=xleaf_sel)
      call select_from_domain(arr_in=leaf_level, ind_sel=ind_sel, arr_out=leaflevel_sel)
@@ -153,12 +195,20 @@ contains
           i = scan(value,'!')
           if (i /= 0) value = trim(adjustl(value(:i-1)))
           select case (trim(name))
-          case ('pos')
-             read(value,*) pos(1),pos(2),pos(3)
-          case ('rvir')
-             read(value,*) rvir
-          case ('typechar')
-             write(typechar,'(a)') trim(value)
+          case('comput_dom_type')
+             write(comput_dom_type,'(a)') trim(value)
+          case ('comput_dom_pos')
+             read(value,*) comput_dom_pos(1),comput_dom_pos(2),comput_dom_pos(3)
+          case ('comput_dom_rsp')
+             read(value,*) comput_dom_rsp
+          case ('comput_dom_rin')
+             read(value,*) comput_dom_rin
+          case ('comput_dom_rout')
+             read(value,*) comput_dom_rout
+          case ('comput_dom_size')
+             read(value,*) comput_dom_size
+          case ('comput_dom_thickness')
+             read(value,*) comput_dom_thickness
           case ('verbose')
              read(value,*) verbose
           case ('DataDir')
@@ -169,23 +219,41 @@ contains
              read(value,*) snapnum
           case('decomp_dom_type')
              write(decomp_dom_type,'(a)') trim(value)
-          case ('ndomain')
+          case ('decomp_dom_ndomain')
              ndomain_present = .true.
-             read(value,*) ndomain
-             allocate(rin(ndomain),rout(ndomain))
-          case ('rin')
-             read(value,*) rin(:)
-          case ('rout')
-             read(value,*) rout(:)
+             read(value,*) decomp_dom_ndomain
+             allocate(decomp_dom_xc(decomp_dom_ndomain),decomp_dom_yc(decomp_dom_ndomain),decomp_dom_zc(decomp_dom_ndomain))
+             allocate(decomp_dom_rin(decomp_dom_ndomain),decomp_dom_rout(decomp_dom_ndomain))
+             allocate(decomp_dom_rsp(decomp_dom_ndomain),decomp_dom_size(decomp_dom_ndomain))
+             allocate(decomp_dom_thickness(decomp_dom_ndomain))
+          case ('decomp_dom_xc')
+             read(value,*) decomp_dom_xc(:)
+          case ('decomp_dom_yc')
+             read(value,*) decomp_dom_yc(:)
+          case ('decomp_dom_zc')
+             read(value,*) decomp_dom_zc(:)
+          case ('decomp_dom_rsp')
+             read(value,*) decomp_dom_rsp(:)
+          case ('decomp_dom_rin')
+             read(value,*) decomp_dom_rin(:)
+          case ('decomp_dom_rout')
+             read(value,*) decomp_dom_rout(:)
+          case ('decomp_dom_size')
+             read(value,*) decomp_dom_size(:)
+          case ('decomp_dom_thickness')
+             read(value,*) decomp_dom_thickness(:)
           end select
        end do
     end if
     close(10)
 
     if (.not. ndomain_present) then ! assign default values
-       allocate(rin(ndomain),rout(ndomain))
-       rin(1)  = 0.0d0
-       rout(1) = min(rvir + 0.05d0, 0.5d0)
+       allocate(decomp_dom_rsp(1))
+       allocate(decomp_dom_xc(1),decomp_dom_yc(1),decomp_dom_zc(1))
+       decomp_dom_rsp(1) = min(comput_dom_rsp + 0.05d0, 0.5d0)
+       decomp_dom_xc(1) = 0.5d0
+       decomp_dom_yc(1) = 0.5d0
+       decomp_dom_zc(1) = 0.5d0
     end if
 
     ! make sure DataDir ends with a /
@@ -215,15 +283,37 @@ contains
        write(unit,'(a,a)')           '  repository      = ',trim(repository)
        write(unit,'(a,i5)')          '  snapnum         = ',snapnum
        write(unit,'(a)')             '# computational domain parameters'
-       write(unit,'(a,a)')           '  typechar        = ',trim(typechar)
-       write(unit,'(a,3(ES10.3,1x))')'  pos             = ',pos(1),pos(2),pos(3)
-       write(unit,'(a,ES10.3)')      '  rvir            = ',rvir
+       write(unit,'(a,a)')           '  comput_dom_type      = ',trim(comput_dom_type)
+       write(unit,'(a,3(ES10.3,1x))')'  comput_dom_pos       = ',comput_dom_pos(1),comput_dom_pos(2),comput_dom_pos(3)
+       select case(comput_dom_type)
+       case ('sphere')
+          write(unit,'(a,ES10.3)')      '  comput_dom_rsp       = ',comput_dom_rsp
+       case ('shell')
+          write(unit,'(a,ES10.3)')      '  comput_dom_rin       = ',comput_dom_rin
+          write(unit,'(a,ES10.3)')      '  comput_dom_rout      = ',comput_dom_rout
+       case ('cube')
+          write(unit,'(a,ES10.3)')      '  comput_dom_size      = ',comput_dom_size
+       case ('slab')
+          write(unit,'(a,ES10.3)')      '  comput_dom_thickness = ',comput_dom_thickness
+       end select
        write(unit,'(a)')             '# domain decomposition parameters'
-       write(unit,'(a,a)')           '  decomp_dom_type = ',trim(decomp_dom_type)
-       write(unit,'(a,i5)')          '  ndomain         = ',ndomain
-       write(fmt,'(a,i3,a)') '(a,',ndomain,'(ES10.3,1x))'
-       write(unit,fmt)               '  rin             = ',rin(:)
-       write(unit,fmt)               '  rout            = ',rout(:)
+       write(unit,'(a,a)')           '  decomp_dom_type      = ',trim(decomp_dom_type)
+       write(unit,'(a,i5)')          '  decomp_dom_ndomain   = ',decomp_dom_ndomain
+       write(fmt,'(a,i3,a)') '(a,',decomp_dom_ndomain,'(ES10.3,1x))'
+       write(unit,fmt)               '  decomp_dom_xc        = ',decomp_dom_xc(:)
+       write(unit,fmt)               '  decomp_dom_yc        = ',decomp_dom_yc(:)
+       write(unit,fmt)               '  decomp_dom_zc        = ',decomp_dom_zc(:)
+       select case(decomp_dom_type)
+       case ('sphere')
+          write(unit,fmt)               '  decomp_dom_rsp       = ',decomp_dom_rsp(:)
+       case ('shell')
+          write(unit,fmt)               '  decomp_dom_rin       = ',decomp_dom_rin(:)
+          write(unit,fmt)               '  decomp_dom_rout      = ',decomp_dom_rout(:)
+       case ('cube')
+          write(unit,fmt)               '  decomp_dom_size      = ',decomp_dom_size(:)
+       case ('slab')
+          write(unit,fmt)               '  decomp_dom_thickness = ',decomp_dom_thickness(:)
+       end select
        write(unit,'(a)')             '# miscelaneous parameters'
        write(unit,'(a,L1)')          '  verbose         = ',verbose
        write(unit,'(a)')             ' '
@@ -235,18 +325,40 @@ contains
        write(*,'(a,a)')           '  repository = ',trim(repository)
        write(*,'(a,i5)')          '  snapnum    = ',snapnum
        write(*,'(a)')             '# computational domain parameters'
-       write(*,'(a,a)')           '  typechar   = ',trim(typechar)
-       write(*,'(a,3(ES10.3,1x))')'  pos        = ',pos(1),pos(2),pos(3)
-       write(*,'(a,ES10.3)')      '  rvir       = ',rvir
+       write(*,'(a,a)')           '  comput_dom_type      = ',trim(comput_dom_type)
+       write(*,'(a,3(ES10.3,1x))')'  comput_dom_pos       = ',comput_dom_pos(1),comput_dom_pos(2),comput_dom_pos(3)
+       select case(comput_dom_type)
+       case ('sphere')
+          write(*,'(a,ES10.3)')      '  comput_dom_rsp       = ',comput_dom_rsp
+       case ('shell')
+          write(*,'(a,ES10.3)')      '  comput_dom_rin       = ',comput_dom_rin
+          write(*,'(a,ES10.3)')      '  comput_dom_rout      = ',comput_dom_rout
+       case ('cube')
+          write(*,'(a,ES10.3)')      '  comput_dom_size      = ',comput_dom_size
+       case ('slab')
+          write(*,'(a,ES10.3)')      '  comput_dom_thickness = ',comput_dom_thickness
+       end select
        write(*,'(a)')             '# domain decomposition parameters'
-       write(*,'(a,a)')           '  decomp_dom_type = ',trim(decomp_dom_type)
-       write(*,'(a,i5)')          '  ndomain         = ',ndomain
-       write(fmt,'(a,i3,a)') '(a,',ndomain,'(ES10.3,1x))'
-       write(*,fmt)               '  rin             = ',rin(:)
-       write(*,fmt)               '  rout            = ',rout(:)
+       write(*,'(a,a)')           '  decomp_dom_type      = ',trim(decomp_dom_type)
+       write(*,'(a,i5)')          '  decomp_dom_ndomain   = ',decomp_dom_ndomain
+       write(fmt,'(a,i3,a)') '(a,',decomp_dom_ndomain,'(ES10.3,1x))'
+       write(*,fmt)               '  decomp_dom_xc        = ',decomp_dom_xc(:)
+       write(*,fmt)               '  decomp_dom_yc        = ',decomp_dom_yc(:)
+       write(*,fmt)               '  decomp_dom_zc        = ',decomp_dom_zc(:)
+       select case(decomp_dom_type)
+       case ('sphere')
+          write(*,fmt)               '  decomp_dom_rsp       = ',decomp_dom_rsp(:)
+       case ('shell')
+          write(*,fmt)               '  decomp_dom_rin       = ',decomp_dom_rin(:)
+          write(*,fmt)               '  decomp_dom_rout      = ',decomp_dom_rout(:)
+       case ('cube')
+          write(*,fmt)               '  decomp_dom_size      = ',decomp_dom_size(:)
+       case ('slab')
+          write(*,fmt)               '  decomp_dom_thickness = ',decomp_dom_thickness(:)
+       end select
        write(*,'(a)')             '# miscelaneous parameters'
-       write(*,'(a,L1)')          '  verbose    = ',verbose
-       write(*,'(a)')             ' '       
+       write(*,'(a,L1)')          '  verbose         = ',verbose
+       write(*,'(a)')             ' '
        call print_mesh_params
     end if
 
