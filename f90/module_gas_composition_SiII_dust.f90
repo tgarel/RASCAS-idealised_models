eee2c524d77f5700854ae469393bb27818265226
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2017-04-11 09:36:37 +0200

    bug fix value of flag in absorption case.

--- f90/module_gas_composition_SiII_dust.f90
+++ f90/module_gas_composition_SiII_dust.f90
@@ -229,7 +229,7 @@ contains
        call scatter_SiII_1193(cell_gas%v, cell_gas%dopwidth, nu_cell, k, nu_ext, iran)
     case(3)
        call scatter_dust(cell_gas%v, nu_cell, k, nu_ext, iran, ilost)
-       if(ilost==1)flag=4
+       if(ilost==1)flag=-1
     end select
 
   end subroutine gas_scatter
