71a1d5e167a736b7877ea84fc32e1b91f1df42c5
Thibault <thibault.garel@univ-lyon1.fr>
2018-04-25 11:09:46 +0200

    Adds sphere_homogen_Vgradient

--- f90/module_idealised_models.f90
+++ f90/module_idealised_models.f90
@@ -28,7 +28,7 @@ module module_idealised_models
 
   
   ! public functions:
-  public :: read_IdealisedModels_params, print_IdealisedModels_params, compute_idealised_gas, shell_V_rho_gradient, shellcone_V_rho_gradient, shell_chisholm, shell_V_rho_gradient_steady, shell_homogen_velfix
+  public :: read_IdealisedModels_params, print_IdealisedModels_params, compute_idealised_gas, shell_V_rho_gradient, shellcone_V_rho_gradient, shell_chisholm, shell_V_rho_gradient_steady, sphere_homogen_velfix, sphere_homogen_Vgradient
   
   
 contains
@@ -65,9 +65,13 @@ contains
        do ileaf=1,nleaf
           call shell_chisholm(n_dust(ileaf),n_gas(ileaf),b_param(ileaf),v_leaf(1,ileaf),v_leaf(2,ileaf),v_leaf(3,ileaf),x_leaf(ileaf,1),x_leaf(ileaf,2),x_leaf(ileaf,3),dx_cell)
        end do
-    case('shell_homogen_velfix')
+    case('sphere_homogen_velfix')
        do ileaf=1,nleaf
-          call shell_homogen_velfix(n_dust(ileaf),n_gas(ileaf),b_param(ileaf),v_leaf(1,ileaf),v_leaf(2,ileaf),v_leaf(3,ileaf),x_leaf(ileaf,1),x_leaf(ileaf,2),x_leaf(ileaf,3),dx_cell)
+          call sphere_homogen_velfix(n_dust(ileaf),n_gas(ileaf),b_param(ileaf),v_leaf(1,ileaf),v_leaf(2,ileaf),v_leaf(3,ileaf),x_leaf(ileaf,1),x_leaf(ileaf,2),x_leaf(ileaf,3),dx_cell)
+       end do
+       case('sphere_homogen_Vgradient')
+       do ileaf=1,nleaf
+          call sphere_homogen_Vgradient(n_dust(ileaf),n_gas(ileaf),b_param(ileaf),v_leaf(1,ileaf),v_leaf(2,ileaf),v_leaf(3,ileaf),x_leaf(ileaf,1),x_leaf(ileaf,2),x_leaf(ileaf,3),dx_cell)
        end do
     end select
     
@@ -473,9 +477,9 @@ contains
   end subroutine shell_chisholm
 
 
-  !!+++++++++++++++++++++++++++++ HOMOGEN SHELL with Fixed Velocity ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  !!+++++++++++++++++++++++++++++ HOMOGEN Sphere with Fixed Velocity ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   
-  subroutine shell_homogen_velfix(ndust_ideal,ngas_ideal,bparam_ideal,vx_ideal,vy_ideal,vz_ideal,xcell_ideal,ycell_ideal,zcell_ideal,dx_cell)
+  subroutine sphere_homogen_velfix(ndust_ideal,ngas_ideal,bparam_ideal,vx_ideal,vy_ideal,vz_ideal,xcell_ideal,ycell_ideal,zcell_ideal,dx_cell)
     
     implicit none
     
@@ -572,8 +576,108 @@ contains
     
     return
     
-  end subroutine shell_homogen_velfix
+  end subroutine sphere_homogen_velfix
   
+   !!+++++++++++++++++++++++++++++ HOMOGEN Sphere with V gradient ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
+  subroutine sphere_homogen_Vgradient(ndust_ideal,ngas_ideal,bparam_ideal,vx_ideal,vy_ideal,vz_ideal,xcell_ideal,ycell_ideal,zcell_ideal,dx_cell)
+    
+    implicit none
+    
+    ! point source at center and medium transparent at R > r_max
+
+    ! Declare arguments
+    real(kind=8)                          :: dx_cell
+    real(kind=8),intent(inout)            :: ndust_ideal,ngas_ideal,bparam_ideal
+    real(kind=8),intent(inout)            :: vx_ideal,vy_ideal,vz_ideal
+    real(kind=8),intent(in)               :: xcell_ideal,ycell_ideal,zcell_ideal    
+    real(kind=8)                          :: volfrac2
+    real(kind=8)                          :: dist_cell,dist2,dist_cell_min,dist_cell_max
+    integer(kind=4)                       :: missed_cell
+    real(kind=8)                          :: n0
+
+    
+    vx_ideal    = 0.0d0
+    vy_ideal    = 0.0d0
+    vz_ideal    = 0.0d0
+    ngas_ideal  = 0.0d0
+    ndust_ideal = 0.0d0
+    volfrac2    = 1.0d0
+    
+    missed_cell = 1 ! =1 if cell doesn't satisfy and if statements... should not happen!
+    
+    ! xcell, ycell and zcell are in frame with origin at bottom-left corner of box
+    dist2 = (xcell_ideal-0.5d0)**2 + (ycell_ideal-0.5d0)**2 + (zcell_ideal-0.5d0)**2  ! in frame with origin at center of box
+    dist_cell = sqrt(dist2)
+    
+    if (coldens_norm .gt. 1.0d0) then 
+       ! if gas norm set as column density in param file.... OK ngas=const. and r_min=0 only !
+       n0 = coldens_norm / (r_max * box_size_IM_cm)  ! cm-3
+    else
+       ! I use the std density ngas_norm
+       n0 = ngas_norm 
+    end if
+    
+    if (MCsampling) then
+       dist_cell_max = dist_cell + dx_cell * sqrt(3.0d0) / 2.0d0 ! dx_cell * sqrt(3.0) / 2. is half the longest length in a cube
+       dist_cell_min = dist_cell - dx_cell * sqrt(3.0d0) / 2.0d0 ! dx_cell * sqrt(3.0) / 2. is half the longest length in a cube
+       
+       if ((dist_cell_max > r_max .and. dist_cell_min < r_max)) then ! cell partially within shell
+          volfrac2 = mc_sampling_shell(xcell_ideal,ycell_ideal,zcell_ideal,dx_cell)
+          missed_cell = 0
+       end if
+       if (dist_cell_max < r_max) then ! cell completely within shell
+          volfrac2 = 1.0
+          missed_cell = 0
+       end if
+       
+       ngas_ideal  = n0 * volfrac2
+       ndust_ideal = ndust_norm * ngas_ideal / n0
+       
+       vx_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (xcell_ideal - 0.5d0)
+       vy_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (ycell_ideal - 0.5d0)
+       vz_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (zcell_ideal - 0.5d0)
+       
+       
+       if (dist_cell_min > r_max) then  ! cell completely out of shell                                               
+          vx_ideal    = 0.0d0
+          vy_ideal    = 0.0d0
+          vz_ideal    = 0.0d0
+          ngas_ideal  = 0.0d0
+          ndust_ideal = 0.0d0
+          missed_cell = 0
+       end if
+       
+    else ! Brut force
+       if (dist_cell < r_max) then ! cell completely within sphere
+          vx_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (xcell_ideal - 0.5d0)
+          vy_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (ycell_ideal - 0.5d0)
+          vz_ideal = Vgas_norm / r_max**(Vgas_slope) * dist_cell**(Vgas_slope-1.0) * (zcell_ideal - 0.5d0)
+          
+          ngas_ideal  = n0 
+          ndust_ideal = ndust_norm * ngas_ideal / n0
+          missed_cell = 0
+       else                                          ! cell completely out of sphere          
+          vx_ideal    = 0.0d0
+          vy_ideal    = 0.0d0
+          vz_ideal    = 0.0d0
+          ngas_ideal  = 0.0d0
+          ndust_ideal = 0.0d0
+          missed_cell = 0
+       end if
+    end if
+    
+    bparam_ideal = sqrt(2.0d0*kb*Tgas_norm)
+    
+    if (missed_cell .eq. 1) then
+       print*,'I missed a cell in module_idealised_models !'
+       print*,dist_cell
+       stop
+    endif
+    
+    return
+    
+  end subroutine sphere_homogen_Vgradient
   
   !!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ END MODELS //////////////////////////////////////////////////////////////////////////
   
