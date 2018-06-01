edeaa6cde310417ef670cdf47f72ce3dccc4a77a
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2018-06-01 16:06:46 +0200

    Added subroutines in module_ramses needed to read only part of CPU files according to the Peano-Hilbert decomposition

--- f90/module_ramses.f90
+++ f90/module_ramses.f90
@@ -166,8 +166,416 @@ contains
     return
 
   end subroutine read_leaf_cells
+  
+  
+  subroutine get_cpu_list(repository, snapnum, xmin,xmax,ymin,ymax,zmin,zmax, ncpu_read, cpu_list)
+    
+    implicit none
+    
+    character(2000),intent(in) :: repository
+    integer(kind=4),intent(in) :: snapnum
+    real(KIND=8),intent(in) :: xmin,xmax,ymin,ymax,zmin,zmax
+    
+    integer(kind=4) :: i,j,ndom
+    integer(kind=4) :: ilevel, lmax, lmin
+    integer(kind=4) :: ncpu_read, ncpu
+    integer(kind=4) :: imin,imax,jmin,jmax,kmin,kmax
+    integer(kind=4) :: impi,bit_length,maxdom
+    integer(kind=4),dimension(1:8):: idom,jdom,kdom,cpu_min,cpu_max
+    real(KIND=8),dimension(1:8):: bounding_min,bounding_max, order_min
+    real(KIND=8)::dkey,dmax
+    real(KIND=8)::dx
+    
+    real(kind=8),dimension(:),allocatable :: bound_key
+    logical,dimension(:),allocatable      :: cpu_read
+    integer(kind=4),dimension(:),allocatable,intent(out)      :: cpu_list
+    
+    lmax = nint(get_param_real(repository,snapnum,'levelmax'))
+    ncpu = get_ncpu(repository,snapnum)
+    
+    dmax = max(xmax-xmin,ymax-ymin,zmax-zmin)
+    
+    allocate(cpu_list(1:ncpu))
+    allocate(bound_key(0:ncpu))
+    allocate(cpu_read(1:ncpu))
+    cpu_read=.false.
+    cpu_list=0
+    
+    call read_hilbert_keys(repository,snapnum,ncpu,bound_key)
+    
+    do ilevel=1,lmax
+       dx=0.5d0**ilevel
+       if(dx.lt.dmax)exit
+    end do
+    lmin=ilevel
+    bit_length=lmin-1
+    maxdom=2**bit_length
+    imin=0; imax=0; jmin=0; jmax=0; kmin=0; kmax=0
+    if(bit_length>0)then
+       imin=int(xmin*dble(maxdom))
+       imax=imin+1
+       jmin=int(ymin*dble(maxdom))
+       jmax=jmin+1
+       kmin=int(zmin*dble(maxdom))
+       kmax=kmin+1
+    endif
+    
+    dkey=(dble(2**(lmax+1)/dble(maxdom)))**ndim
+    ndom=1
+    if(bit_length>0)ndom=8
+    idom(1)=imin; idom(2)=imax
+    idom(3)=imin; idom(4)=imax
+    idom(5)=imin; idom(6)=imax
+    idom(7)=imin; idom(8)=imax
+    jdom(1)=jmin; jdom(2)=jmin
+    jdom(3)=jmax; jdom(4)=jmax
+    jdom(5)=jmin; jdom(6)=jmin
+    jdom(7)=jmax; jdom(8)=jmax
+    kdom(1)=kmin; kdom(2)=kmin
+    kdom(3)=kmin; kdom(4)=kmin
+    kdom(5)=kmax; kdom(6)=kmax
+    kdom(7)=kmax; kdom(8)=kmax
+    
+    do i=1,ndom
+       if(bit_length>0)then
+          call hilbert3d(idom(i),jdom(i),kdom(i),order_min(i),bit_length,1)
+       else
+          order_min(i)=0.0d0
+       endif
+       bounding_min(i)=(order_min(i))*dkey
+       bounding_max(i)=(order_min(i)+1.0D0)*dkey
+    end do
+    
+    cpu_min=0; cpu_max=0
+    do impi=1,ncpu
+       do i=1,ndom
+          if (   bound_key(impi-1).le.bounding_min(i).and.&
+               & bound_key(impi  ).gt.bounding_min(i))then
+             cpu_min(i)=impi
+          endif
+          if (   bound_key(impi-1).lt.bounding_max(i).and.&
+               & bound_key(impi  ).ge.bounding_max(i))then
+             cpu_max(i)=impi
+          endif
+       end do
+    end do
+    
+    ncpu_read=0
+    do i=1,ndom
+       do j=cpu_min(i),cpu_max(i)
+          if(.not. cpu_read(j))then
+             ncpu_read=ncpu_read+1
+             cpu_list(ncpu_read)=j
+             cpu_read(j)=.true.
+          endif
+       enddo
+    enddo
+    
+    deallocate(bound_key,cpu_read)
+    
+    print*,'--> Ncpu to read = ',ncpu_read
+    
+    return
+    
+  end subroutine get_cpu_list
+  
+  
+  subroutine get_cpu_list_periodic(repository, snapnum, xmin, xmax, ymin &
+       ,ymax, zmin, zmax, ncpu_read, cpu_list)
+    
+    implicit none
+    
+    character(2000),intent(in) :: repository
+    integer(kind=4),intent(in) :: snapnum
+    real(KIND=8),intent(in) :: xmin,xmax,ymin,ymax,zmin,zmax
+    
+    ! Can have dom_min<0 and dom_max>1
+    real(KIND=8),dimension(3)::     dom_min,dom_max
+    ! Can NOT have dom_pmin<0 and dom_pmax>1
+    ! (therefore extra domains across boundaires)
+    real(KIND=8),dimension(3,1:2):: dom_pmin,dom_pmax
+    integer(kind=4) :: i,j,ix,iy,iz,ndom
+    integer(kind=4) :: ilevel, lmax, lmin
+    integer(kind=4) :: ncpu_read, ncpu
+    integer(kind=4) :: imin,imax,jmin,jmax,kmin,kmax
+    integer(kind=4) :: impi,bit_length,maxdom
+    integer(kind=4),dimension(1:8):: idom,jdom,kdom,cpu_min,cpu_max
+    real(KIND=8),dimension(1:8):: bounding_min,bounding_max, order_min
+    real(KIND=8)::dkey,dmin,dmax
+    real(KIND=8)::dx
+    
+    real(kind=8),dimension(:),allocatable :: bound_key
+    logical,dimension(:),allocatable      :: cpu_read
+    integer(kind=4),dimension(:),allocatable,intent(out)      :: cpu_list
+    
+    lmax = nint(get_param_real(repository,snapnum,'levelmax'))
+    ncpu = get_ncpu(repository,snapnum)
+    
+    allocate(cpu_list(1:ncpu))
+    allocate(bound_key(0:ncpu))
+    allocate(cpu_read(1:ncpu))
+    cpu_read=.false.
+    cpu_list=0
+    
+    print*,'...getting CPU list...'
+    
+    call read_hilbert_keys(repository,snapnum,ncpu,bound_key)
+    
+    ! Set up the periodic domains
+    dom_min(1) = xmin;   dom_max(1) = xmax
+    dom_min(2) = ymin;   dom_max(2) = ymax
+    dom_min(3) = zmin;   dom_max(3) = zmax
+    dom_pmin(:,1) = dom_min ! Main domain ...
+    dom_pmax(:,1) = dom_max ! ...always used
+    dom_pmin(:,2) = 1.
+    dom_pmax(:,2) = 0.
+    do i=1,3 ! loop x,y,z
+       if (dom_min(i) < 0.) then ! Lower limit left of boundary
+          dom_pmin(i,1) = 0.     ! Adjust main domain
+          dom_pmin(i,2) = 1. + dom_min(i) ! Extra domain
+          dom_pmax(i,2) = 1.
+       endif
+       if (dom_max(i) > 1.) then ! Upper limit right of boundary
+          dom_pmax(i,1) = 1.     ! Adjust main domain
+          dom_pmax(i,2) = dom_min(i) - 1. ! Extra domain
+          dom_pmin(i,2) = 0.
+       endif
+    end do
+    
+    ncpu_read=0
+    do ix=1,2
+    do iy=1,2
+    do iz=1,2
+       
+       dmin = min(dom_pmax(1,ix)-dom_pmin(1,ix)  &
+                 ,dom_pmax(2,iy)-dom_pmin(2,iy)  &
+                 ,dom_pmax(3,iz)-dom_pmin(3,iz) )
+       if(dmin<=0.) cycle
+       !print*,'Extracting from hilbert domain '
+       !print*,xmin,xmax
+       !print*,ymin,ymax
+       !print*,zmin,zmax
+       !print*,dom_pmin(1,ix),dom_pmax(1,ix)
+       !print*,dom_pmin(2,iy),dom_pmax(2,iy)
+       !print*,dom_pmin(3,iz),dom_pmax(3,iz)
+       !dmax = max(xmax-xmin,ymax-ymin,zmax-zmin)
+       dmax = max(dom_pmax(1,ix)-dom_pmin(1,ix)  &
+                 ,dom_pmax(2,iy)-dom_pmin(2,iy)  &
+                 ,dom_pmax(3,iz)-dom_pmin(3,iz) )
 
-
+       ! Set up at most 8 sub-domains in periodic box
+       do ilevel=1,lmax
+          dx=0.5d0**ilevel
+          if(dx.lt.dmax)exit
+       end do
+       lmin=ilevel
+       bit_length=lmin-1
+       maxdom=2**bit_length
+       imin=0; imax=0; jmin=0; jmax=0; kmin=0; kmax=0
+       if(bit_length>0)then
+          imin=int(dom_pmin(1,ix)*dble(maxdom))
+          imax=imin+1
+          jmin=int(dom_pmin(2,iy)*dble(maxdom))
+          jmax=jmin+1
+          kmin=int(dom_pmin(3,iz)*dble(maxdom))
+          kmax=kmin+1
+       endif
+       
+       dkey=(dble(2**(lmax+1)/dble(maxdom)))**ndim
+       ndom=1
+       if(bit_length>0)ndom=8
+       idom(1)=imin; idom(2)=imax
+       idom(3)=imin; idom(4)=imax
+       idom(5)=imin; idom(6)=imax
+       idom(7)=imin; idom(8)=imax
+       jdom(1)=jmin; jdom(2)=jmin
+       jdom(3)=jmax; jdom(4)=jmax
+       jdom(5)=jmin; jdom(6)=jmin
+       jdom(7)=jmax; jdom(8)=jmax
+       kdom(1)=kmin; kdom(2)=kmin
+       kdom(3)=kmin; kdom(4)=kmin
+       kdom(5)=kmax; kdom(6)=kmax
+       kdom(7)=kmax; kdom(8)=kmax
+       
+       do i=1,ndom
+          if(bit_length>0)then
+             call hilbert3d(idom(i),jdom(i),kdom(i),order_min(i),bit_length,1)
+          else
+             order_min(i)=0.0d0
+          endif
+          bounding_min(i)=(order_min(i))*dkey
+          bounding_max(i)=(order_min(i)+1.0D0)*dkey
+       end do
+       
+       cpu_min=0; cpu_max=0
+       do impi=1,ncpu
+          do i=1,ndom
+             if (   bound_key(impi-1).le.bounding_min(i).and.&
+                  & bound_key(impi  ).gt.bounding_min(i))then
+                cpu_min(i)=impi
+             endif
+             if (   bound_key(impi-1).lt.bounding_max(i).and.&
+                  & bound_key(impi  ).ge.bounding_max(i))then
+                cpu_max(i)=impi
+             endif
+          end do
+       end do
+       
+       do i=1,ndom
+          do j=cpu_min(i),cpu_max(i)
+             if(.not. cpu_read(j))then
+                ncpu_read=ncpu_read+1
+                cpu_list(ncpu_read)=j
+                cpu_read(j)=.true.
+             endif
+          enddo
+       enddo
+       
+    end do !ix=1,2
+    end do !iy=1,2
+    end do !iz=1,2
+    
+    deallocate(bound_key,cpu_read)
+    
+    print*,'--> Ncpu to read = ',ncpu_read
+    
+    return
+    
+  end subroutine get_cpu_list_periodic
+  
+  
+  subroutine read_hilbert_keys(repository,snapnum,ncpu,bound_key)
+    
+    implicit none
+    
+    character(2000),intent(in)                   :: repository
+    integer(kind=4),intent(in)                   :: snapnum, ncpu
+    real(kind=8),dimension(0:ncpu),intent(inout) :: bound_key
+    
+    logical(kind=4)            :: not_ok
+    character(512)             :: nomfich
+    character(512)             :: line,name,value,orderingtype
+    integer(kind=4)            :: i, impi
+    integer(kind=4),parameter  :: param_unit = 13
+    
+    not_ok = .true.
+    write(nomfich,'(a,a,i5.5,a,i5.5,a)') trim(repository),'/output_',snapnum,'/info_',snapnum,'.txt'
+    open(unit=param_unit,file=nomfich,status='old',form='formatted')
+    do
+       read(param_unit,'(a)',end=2) line
+       i = scan(line,'=')
+       if (i==0 .or. line(1:1)=='#') cycle
+       name=trim(adjustl(line(:i-1)))
+       value=trim(adjustl(line(i+1:)))
+       ! check for a comment at end of line !                                                                                
+       i = scan(value,'!')
+       if (i /= 0) value = trim(adjustl(value(:i-1)))
+       if (trim(name) .eq. 'ordering type') then
+          read(value,*) orderingtype
+          if (orderingtype=='hilbert') then
+             ! start reading keys                                                                                            
+             ! skip one line                                                                                                 
+             read(param_unit,*)             
+             do impi=1,ncpu
+                read(param_unit,'(I8,1X,E23.15,1X,E23.15)')i,bound_key(impi-1),bound_key(impi)
+             end do
+          end if
+       end if
+    end do
+2   close (param_unit)
+    
+    return
+    
+  end subroutine read_hilbert_keys
+  
+  
+  subroutine hilbert3d(x,y,z,order,bit_length,npoint)
+    implicit none
+    
+    integer     ,INTENT(IN)                     ::bit_length,npoint
+    integer     ,INTENT(IN) ,dimension(1:npoint)::x,y,z
+    real(kind=8),INTENT(OUT),dimension(1:npoint)::order
+    
+    logical,dimension(0:3*bit_length-1)::i_bit_mask
+    logical,dimension(0:1*bit_length-1)::x_bit_mask,y_bit_mask,z_bit_mask
+    integer,dimension(0:7,0:1,0:11)::state_diagram
+    integer::i,ip,cstate,nstate,b0,b1,b2,sdigit,hdigit
+    
+    if(bit_length>bit_size(bit_length))then
+       write(*,*)'Maximum bit length=',bit_size(bit_length)
+       write(*,*)'stop in hilbert3d'
+       stop
+    endif
+    
+    state_diagram = RESHAPE( (/   1, 2, 3, 2, 4, 5, 3, 5,&
+         &   0, 1, 3, 2, 7, 6, 4, 5,&
+         &   2, 6, 0, 7, 8, 8, 0, 7,&
+         &   0, 7, 1, 6, 3, 4, 2, 5,&
+         &   0, 9,10, 9, 1, 1,11,11,&
+         &   0, 3, 7, 4, 1, 2, 6, 5,&
+         &   6, 0, 6,11, 9, 0, 9, 8,&
+         &   2, 3, 1, 0, 5, 4, 6, 7,&
+         &  11,11, 0, 7, 5, 9, 0, 7,&
+         &   4, 3, 5, 2, 7, 0, 6, 1,&
+         &   4, 4, 8, 8, 0, 6,10, 6,&
+         &   6, 5, 1, 2, 7, 4, 0, 3,&
+         &   5, 7, 5, 3, 1, 1,11,11,&
+         &   4, 7, 3, 0, 5, 6, 2, 1,&
+         &   6, 1, 6,10, 9, 4, 9,10,&
+         &   6, 7, 5, 4, 1, 0, 2, 3,&
+         &  10, 3, 1, 1,10, 3, 5, 9,&
+         &   2, 5, 3, 4, 1, 6, 0, 7,&
+         &   4, 4, 8, 8, 2, 7, 2, 3,&
+         &   2, 1, 5, 6, 3, 0, 4, 7,&
+         &   7, 2,11, 2, 7, 5, 8, 5,&
+         &   4, 5, 7, 6, 3, 2, 0, 1,&
+         &  10, 3, 2, 6,10, 3, 4, 4,&
+         &   6, 1, 7, 0, 5, 2, 4, 3 /), &
+         & (/8 ,2, 12 /) )
+    
+    do ip=1,npoint
+       
+       ! convert to binary
+       do i=0,bit_length-1
+          x_bit_mask(i)=btest(x(ip),i)
+          y_bit_mask(i)=btest(y(ip),i)
+          z_bit_mask(i)=btest(z(ip),i)
+       enddo
+       
+       ! interleave bits
+       do i=0,bit_length-1
+          i_bit_mask(3*i+2)=x_bit_mask(i)
+          i_bit_mask(3*i+1)=y_bit_mask(i)
+          i_bit_mask(3*i  )=z_bit_mask(i)
+       end do
+       
+       ! build Hilbert ordering using state diagram
+       cstate=0
+       do i=bit_length-1,0,-1
+          b2=0 ; if(i_bit_mask(3*i+2))b2=1
+          b1=0 ; if(i_bit_mask(3*i+1))b1=1
+          b0=0 ; if(i_bit_mask(3*i  ))b0=1
+          sdigit=b2*4+b1*2+b0
+          nstate=state_diagram(sdigit,0,cstate)
+          hdigit=state_diagram(sdigit,1,cstate)
+          i_bit_mask(3*i+2)=btest(hdigit,2)
+          i_bit_mask(3*i+1)=btest(hdigit,1)
+          i_bit_mask(3*i  )=btest(hdigit,0)
+          cstate=nstate
+       enddo
+       
+       ! save Hilbert key as double precision real
+       order(ip)=0.
+       do i=0,3*bit_length-1
+          b0=0 ; if(i_bit_mask(i))b0=1
+          order(ip)=order(ip)+dble(b0)*dble(2)**i
+       end do
+       
+    end do
+    
+  end subroutine hilbert3d
+  
+  
   function get_nGridTot(repository,snapnum)
 
     ! get total number of grids in the simulation 
@@ -1440,7 +1848,7 @@ contains
                    ! convert from tborn to age in Myr
                    star_age(ilast)   = max(0.d0, (time_cu - age(i)) * dp_scale_t / (365.d0*24.d0*3600.d0*1.d6))
                 endif
-                 if (use_initial_mass) then 
+                if (use_initial_mass) then 
                    star_mass(ilast) = imass(i) * dp_scale_m ! [g]
                 else
                    star_mass(ilast) = m(i)     * dp_scale_m ! [g]
