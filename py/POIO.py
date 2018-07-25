29e411e3c03d5377c71c26a6838156e483064ea5
jeremy blaizot <jeremy.blaizot@univ-lyon1.fr>
2018-07-25 23:16:14 +0200

    added mockObs class for reading mocks generated with peeling off. [see py/POIO.py]

---
+++ py/POIO.py
@@ -0,0 +1,102 @@
+class mockobs(object):
+    def __init__(self,Dir,FileName,ncpu,load_spectrum=False,load_image=False,load_cube=False):
+        self.Dir      = Dir
+        self.FileName = FileName 
+        self.ncpu     = ncpu
+
+        import jphot as jp
+        ICFile = FileName.replace('result.','')
+        p = jp.photonlist("%s/%s"%(self.Dir,ICFile),'',load=False)
+        p.load_ic()
+        self.nPhotPerPacket = p.nRealPhotons / p.nphoton
+        import lya_utils as ly
+        self.LumPerPacket = self.nPhotPerPacket * ly.h_cgs * ly.nu0 
+
+        if load_image:
+            self.imsize_cu, self.image_npix, self.imtot = self.__read_image()
+            self.imtot = self.imtot * self.LumPerPacket # [erg/s]
+
+        if load_spectrum:
+            self.lmin,self.lmax,self.spec_npix,self.spectot = self.__read_spectrum()
+
+        if load_cube:
+            self.nlbda,self.nxy,self.lmin,self.lmax,self.imsize,self.cubetot = self.__read_cube()
+        
+    def __read_cube(self):
+        from scipy.io import FortranFile as ff
+        first = True
+        for i in range(1,self.ncpu):
+            f = ff('%s/%s_cube.%5.5i'%(self.Dir,self.FileName,i))
+            nlbda,nxy = f.read_ints()
+            lmin,lmax,imsize = f.read_reals('d')
+            center = f.read_reals('d')
+            cube = f.read_reals('d')
+            cube = cube.reshape((nxy,nxy,nlbda))
+            f.close()
+            if first:
+                cubetot = cube
+                first=False
+            else:
+                cubetot = cubetot + cube
+        return nlbda,nxy,lmin,lmax,imsize,cubetot
+
+    def __read_image(self):
+        from scipy.io import FortranFile as ff
+        first = True
+        for i in range(1,self.ncpu):
+            f = ff('%s/%s_image.%5.5i'%(self.Dir,self.FileName,i))
+            n = f.read_ints()[0]
+            imsize = f.read_reals('d')[0]
+            imcenter = f.read_reals('d')
+            im = f.read_reals('d')
+            im=im.reshape((n,n))
+            f.close()
+            if first:
+                imtot = im
+                first=False
+            else:
+                imtot = imtot + im
+        return imsize,n,imtot 
+
+
+    def __read_spectrum(self):
+        from scipy.io import FortranFile as ff
+        first = True
+        for i in range(1,self.ncpu):
+            f = ff('%s/%s_spectrum.%5.5i'%(self.Dir,self.FileName,i))
+            n = f.read_ints()[0]
+            lmin,lmax = f.read_reals('d')
+            spec = f.read_reals('d')
+            f.close()
+            if first:
+                spectot = spec
+                first=False
+            else:
+                spectot = spectot + spec
+        return lmin,lmax,n,spectot
+
+    
+    def show_image(self,plotFile,vmin=1e-15,vmax=10,showCB=True):
+        import matplotlib
+        matplotlib.use('Agg')
+        from matplotlib import pyplot as plt
+        from matplotlib.colors import LogNorm
+        plt.figure(figsize=(10,10))
+        plt.imshow(self.imtot,norm=LogNorm(),vmin=vmin,vmax=vmax)
+        if showCB :
+            plt.colorbar()    
+        plt.savefig(plotFile)
+
+    def show_spectrum(self,plotFile):
+        import lya_utils as ly
+        import matplotlib
+        matplotlib.use('Agg')
+        from matplotlib import pyplot as plt
+        from numpy import linspace
+        plt.figure()
+        l = linspace(self.lmin,self.lmax,num=self.spec_npix)  # [A]
+        plt.plot(l,self.spectot)
+        plt.savefig(plotFile)
+
+
+
