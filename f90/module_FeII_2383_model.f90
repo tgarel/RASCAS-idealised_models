9a76a2bb895b71cb0cb0645c85eaa160b2b3b0dc
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2018-09-03 16:10:31 +0200

    Updated module FeII 2383.

--- f90/module_FeII_2383_model.f90
+++ f90/module_FeII_2383_model.f90
@@ -23,11 +23,11 @@ module module_FeII_2383_model ! Fe_II UV2
   real(kind=8),parameter :: lambda13_cm    = lambda13 / cmtoA         ! [cm]
   real(kind=8),parameter :: nu13           = clight / lambda13_cm     ! [Hz]
   real(kind=8),parameter :: f13            = 3.2d-1                  ! oscillator strength
-  real(kind=8),parameter :: sigma13_factor = pi*e_ch**2*f13/me/clight ! multiply by Voigt(x,a)/nu_D to get sigma.
+  real(kind=8),parameter :: sigma13_factor = pi*e_ch**2*f13/me/clight ! multiply by Voigt(x,a)/delta_nu_doppler to get sigma.
   real(kind=8),parameter :: A31            = 3.13d8                   ! spontaneous decay [/s]
 
   
-  public :: get_tau_FeII_2383, scatter_FeII_2383
+  public :: get_tau_FeII_2383, scatter_FeII_2383, read_FeII_2383_params, print_FeII_2383_params
 
 contains
 
@@ -46,16 +46,16 @@ contains
     ! --------------------------------------------------------------------------
     
     real(kind=8),intent(in) :: nFeII,vth,distance_to_border_cm,nu_cell
-    real(kind=8)            :: nu_D,x_cell,sigma,a,h,get_tau_FeII_2383
+    real(kind=8)            :: delta_nu_doppler,x_cell,sigma,a,h,get_tau_FeII_2383
 
     ! compute Doppler width and a-parameter
-    nu_D = vth / lambda13_cm
-    a    = A31 / (fourpi * nu_D)
+    delta_nu_doppler = vth / lambda13_cm
+    a = A31 / (fourpi * delta_nu_doppler)
 
     ! cross section of FeII-2382.76
-    x_cell = (nu_cell - nu13) / nu_D
+    x_cell = (nu_cell - nu13) / delta_nu_doppler
     h      = voigt_fit(x_cell,a)
-    sigma  = sigma13_factor / nu_D * h
+    sigma  = sigma13_factor / delta_nu_doppler * h
 
     get_tau_FeII_2383 = sigma * nFeII * distance_to_border_cm
    
@@ -89,7 +89,7 @@ contains
     real(kind=8),dimension(3),intent(in)    :: vcell
     real(kind=8),intent(in)                 :: vth
     integer(kind=4),intent(inout)           :: iran
-    real(kind=8)                            :: delta_nu_doppler, a, x_cell, blah, upar, ruper
+    real(kind=8)                            :: delta_nu_doppler, a, x_cell, upar, ruper
     real(kind=8)                            :: r2, uper, nu_atom, mu, bu, scalar
     real(kind=8),dimension(3)               :: knew
 
@@ -100,12 +100,7 @@ contains
 
     ! 1/ component parallel to photon's propagation
     ! -> get velocity of interacting atom parallel to propagation
-    blah = ran3(iran)
-#ifdef SWITCH_OFF_UPARALLEL
-    upar = 0.
-#else
-    upar = get_uparallel(a,x_cell,blah)
-#endif
+    upar = get_uparallel(x_cell,a,iran)
     upar = upar * vth    ! upar is an x -> convert to a velocity 
 
     ! 2/ component perpendicular to photon's propagation
@@ -131,4 +126,41 @@ contains
 
   end subroutine scatter_FeII_2383
 
+
+  subroutine read_FeII_2383_params(pfile)
+    
+    ! ---------------------------------------------------------------------------------
+    ! subroutine which reads parameters of current module in the parameter file pfile
+    !
+    ! default parameter values are set at declaration (head of module)
+    ! ---------------------------------------------------------------------------------
+    
+    character(*),intent(in) :: pfile
+    
+    call read_uparallel_params(pfile)
+    
+    return
+    
+  end subroutine read_FeII_2383_params
+
+
+  subroutine print_FeII_2383_params(unit)
+    
+    ! ---------------------------------------------------------------------------------
+    ! write parameter values to std output or to an open file if argument unit is
+    ! present.
+    ! ---------------------------------------------------------------------------------
+    
+    integer(kind=4),optional,intent(in) :: unit
+    
+    if (present(unit)) then 
+       call print_uparallel_params(unit)
+    else
+       call print_uparallel_params()
+    end if
+    
+    return
+    
+  end subroutine print_FeII_2383_params
+  
 end module module_FeII_2383_model
