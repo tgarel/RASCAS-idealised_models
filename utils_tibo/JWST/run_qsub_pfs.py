90bfb667acb4c3f068f1c504e90b32b1e9921b52
Thibault <thibault.garel@univ-lyon1.fr>
2018-11-14 15:45:47 +0100

    Corrects paths in run_qsub_ files

--- utils_tibo/JWST/run_qsub_pfs.py
+++ utils_tibo/JWST/run_qsub_pfs.py
@@ -4,13 +4,15 @@ import os
 ################################################################################
 
 halo_ids = ['11518','12763','18546','23233','24695','41361','47123','49444','49905','6114']
+timestep = '00183'
 
 path2confFiles = '/scratch/garel/rascas_sphinx/output/test_Mstar_gt1e-3/'
     
 for j in range(len(halo_ids)):
     # PFS 
-    pfs_dir = "%s%s%s%s"%(path2confFiles,'halo',halo_ids[j],'/1500A_rf/')
-    pfs_cmd = "%s%s%s"%('qsub ',pfs_dir,'/001.PFS.conf.pbscript')
+    pfs_dir = "%s%s%s%s%s"%(path2confFiles,timestep,'/halo',halo_ids[j],'/1500A_rf/')
+    pfs_cmd = "%s%s%s"%('qsub ',pfs_dir,'001.PFS.conf.pbscript')
+    print(pfs_cmd)
     os.system(pfs_cmd)
   
 
--- utils_tibo/JWST/run_qsub_rascas.py
+++ utils_tibo/JWST/run_qsub_rascas.py
@@ -4,14 +4,17 @@ import os
 ################################################################################
 
 halo_ids = ['11518','12763','18546','23233','24695','41361','47123','49444','49905','6114']
+timestep = '00183'
 
 path2confFiles = '/scratch/garel/rascas_sphinx/output/test_Mstar_gt1e-3/'
 
 for j in range(len(halo_ids)):
     # RASCAS 
-    rascas_dir = "%s%s%s%s"%(path2confFiles,'halo',halo_ids[j],'/1500A_rf/')  
-    rascas_cmd = "%s%s%s"%('qsub ',rascas_dir,'/001.RASCAS.conf.pbscript')  
+    rascas_dir = "%s%s%s%s%s"%(path2confFiles,timestep,'/halo',halo_ids[j],'/1500A_rf/')  
+    rascas_cmd = "%s%s%s"%('qsub ',rascas_dir,'001.RASCAS.conf.pbscript')
+    print(rascas_cmd)
     os.system(rascas_cmd)
+
 
 ################################################################################
 
