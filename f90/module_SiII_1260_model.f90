28c57a1481fd932bab9bcc4992683f51716ab874
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2018-11-19 14:15:33 +0100

    Corrected some problems with new voigt module + homogenized notations.

--- f90/module_SiII_1260_model.f90
+++ f90/module_SiII_1260_model.f90
@@ -1,16 +1,15 @@
 module module_SiII_1260_model
 
-  !TIBO
-
   ! This module describes the absorption of photons by SiII from level  3s^2 3p 2P^0 1/2 to level 3s^2 3d 2D 3/2.
   ! This transition is at 1260.42 A.
   ! The module also implements the two decay channels (resonant and fluorescent) at 1260.42 A and 1265.02 A. 
   
   use module_constants
-  use module_utils, only : voigt_fit, isotropic_direction
+  use module_utils, only : isotropic_direction
   use module_uparallel
   use module_random
-
+  use module_voigt
+  
   implicit none
 
   private
@@ -22,18 +21,18 @@ module module_SiII_1260_model
   ! level 3 is 3s^2 3d 2D 3/2
 
   ! transition between levels 1 and 3 
-  real(kind=8),parameter :: lambda13       = 1260.42d0                ! transition wavelength [A]
-  real(kind=8),parameter :: lambda13_cm    = lambda13 / cmtoA         ! [cm]
-  real(kind=8),parameter :: nu13           = clight / lambda13_cm     ! [Hz]
-  real(kind=8),parameter :: f13            = 1.226d0                  ! oscillator strength
-  real(kind=8),parameter :: sigma13_factor = pi*e_ch**2*f13/me/clight ! multiply by Voigt(x,a)/nu_D to get sigma.
-  real(kind=8),parameter :: A31            = 2.57d9                   ! spontaneous decay [/s]
+  real(kind=8),parameter :: lambda13       = 1260.42d0                    ! transition wavelength [A]
+  real(kind=8),parameter :: lambda13_cm    = lambda13 / cmtoA             ! [cm]
+  real(kind=8),parameter :: nu13           = clight / lambda13_cm         ! [Hz]
+  real(kind=8),parameter :: f13            = 1.226d0                      ! oscillator strength
+  real(kind=8),parameter :: sigma13_factor = sqrtpi*e_ch**2*f13/me/clight ! multiply by Voigt(x,a)/delta_nu_doppler to get sigma.
+  real(kind=8),parameter :: A31            = 2.57d9                       ! spontaneous decay [/s]
 
   ! transition between levels 2 and 3
-  real(kind=8),parameter :: lambda23       = 1265.02d0                ! transition wavelength [A]
-  real(kind=8),parameter :: lambda23_cm    = lambda23 / cmtoA         ! [cm]
-  real(kind=8),parameter :: nu23           = clight / lambda23_cm     ! [Hz]
-  real(kind=8),parameter :: A32            = 4.73d8                   ! spontaneous decay [/s]
+  real(kind=8),parameter :: lambda23       = 1265.02d0                    ! transition wavelength [A]
+  real(kind=8),parameter :: lambda23_cm    = lambda23 / cmtoA             ! [cm]
+  real(kind=8),parameter :: nu23           = clight / lambda23_cm         ! [Hz]
+  real(kind=8),parameter :: A32            = 4.73d8                       ! spontaneous decay [/s]
     
   real(kind=8),parameter :: Atot = A31+A32
 
@@ -58,16 +57,16 @@ contains
     ! --------------------------------------------------------------------------
     
     real(kind=8),intent(in) :: nSiII,vth,distance_to_border_cm,nu_cell
-    real(kind=8)            :: nu_D,x_cell,sigma,a,h,get_tau_SiII_1260
+    real(kind=8)            :: delta_nu_doppler,x_cell,sigma,a,h_cell,get_tau_SiII_1260
 
     ! compute Doppler width and a-parameter
-    nu_D = vth / lambda13_cm
-    a    = A31 / (fourpi * nu_D)
+    delta_nu_doppler = vth / lambda13_cm
+    a = A31 / (fourpi * delta_nu_doppler)
 
     ! cross section of SiII-1260.42
-    x_cell = (nu_cell - nu13) / nu_D
-    h      = voigt_fit(x_cell,a)
-    sigma  = sigma13_factor / nu_D * h
+    x_cell = (nu_cell - nu13) / delta_nu_doppler
+    h_cell = voigt_function(x_cell,a)
+    sigma  = sigma13_factor / delta_nu_doppler * h_cell
 
     get_tau_SiII_1260 = sigma * nSiII * distance_to_border_cm
    
@@ -157,6 +156,7 @@ contains
     character(*),intent(in) :: pfile
 
     call read_uparallel_params(pfile)
+    call read_voigt_params(pfile)
 
     return
 
@@ -174,8 +174,10 @@ contains
 
     if (present(unit)) then 
        call print_uparallel_params(unit)
+       call print_voigt_params(unit)
     else
        call print_uparallel_params()
+       call print_voigt_params()
     end if
     
     return
