dc6cd8cd58fe457f93ce84f024b988022f8cb75b
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2019-03-05 17:43:30 +0100

    Update PFS for new module_ssp_lib + 1st try for the table case.

--- f90/PhotonsFromStars.f90
+++ f90/PhotonsFromStars.f90
@@ -9,7 +9,6 @@ program PhotonsFromStars
   use module_ramses
 
   use module_ssp_lib
-  use class_twod_table
   
   implicit none
   
@@ -17,25 +16,24 @@ program PhotonsFromStars
   character(2000) :: parameter_file
   real(kind=8),allocatable :: star_pos(:,:),star_age(:),star_mass(:),star_vel(:,:),star_met(:)
   integer(kind=4) :: iran,i,nstars,narg
-  integer(kind=8) :: ilast,j
-  real(kind=8)    :: scalar,nu,r1,r2
+  !!integer(kind=8) :: ilast,j
+  real(kind=8)    :: scalar,r1,r2
   ! for analysis purposes (a posteriori weighting) we want to save the emitter-frame
   ! frequency (here the freq. in the emitting stellar particle's frame)
   real(kind=8),allocatable    :: nu_star(:)
   ! SED-related variables
-  integer(kind=4)             :: sed_nage,sed_nmet,imet,iage
-  integer(kind=8)             :: nflux,n,ix
-  real(kind=8),allocatable    :: sed_age(:),sed_met(:),sweight(:),sed_nphot(:,:),sed_F_0(:,:),sed_beta(:,:)
-  integer(kind=8),allocatable :: cum_flux_prob(:)
-  integer(kind=4),allocatable :: star_iage(:),star_imet(:)
-  real(kind=8),allocatable    :: star_beta(:) 
-  real(kind=8)                :: total_flux,minflux,check_flux,f0,beta,betaplus2,lambda,x,dx1,dx2,dx
-  real(kind=8) :: dxage1,dxmet1,w,dxage2,dxmet2
+  !!!integer(kind=4)             :: sed_nage,sed_nmet,imet,iage
+  !!integer(kind=8)             :: nflux,n,ix
+  !!!real(kind=8),allocatable    :: sed_age(:),sed_met(:),sweight(:),sed_nphot(:,:),sed_F_0(:,:),sed_beta(:,:)
+  !integer(kind=4),allocatable :: star_iage(:),star_imet(:)
+  !real(kind=8),allocatable    :: star_beta(:) 
+  real(kind=8)                :: total_flux   !!!,minflux,check_flux,f0 !!,beta,betaplus2,lambda,x,dx1,dx2,dx
+  !real(kind=8) :: dxage1,dxmet1,w,dxage2,dxmet2
 
-  type(twod_table) :: Ndot
-  real(kind=8),allocatable :: low_prob(:), x_em(:,:), k_em(:,:), nu_em(:) 
-  integer(kind=4) :: ilow, iup, imid, iphot, iseed, nsel 
-  real(kind=8) :: lambda0, nphotPerSecPerMsun(1), mid, k(3)
+  type(SSPgrid) :: NdotGrid
+  real(kind=8),allocatable :: low_prob(:), low_prob2(:), x_em(:,:), k_em(:,:), nu_em(:), Ndot(:), NdotStar(:,:), sweight(:)
+  integer(kind=4) :: ilow, iup, imid, iphot, iseed, ilow2, iup2, imid2
+  real(kind=8) :: lambda0, mid, k(3), lambdamin, lambdamax, mid2
 
   
   ! --------------------------------------------------------------------------
@@ -72,8 +70,8 @@ program PhotonsFromStars
   real(kind=8)              :: spec_powlaw_lmin_Ang = 1120.  ! min wavelength to sample (should be in the range where fit was made ...)
   real(kind=8)              :: spec_powlaw_lmax_Ang = 1320.  ! max ...
   ! parameters for spec_type == 'Table'
-  integer(kind=4)           :: spec_table_nbins       ! read from the weight file
-  real(kind=8),allocatable  :: spec_table_lofx(:,:,:) ! -> allocated to (spec_table_nbins,sed_nage,sed_nmet)
+  real(kind=8)              :: spec_table_lmin_Ang = 1120.  ! min wavelength to sample
+  real(kind=8)              :: spec_table_lmax_Ang = 1320.  ! max ...
 
   ! --- parameters for star particles/feedback in simulation
   real(kind=8)              :: tdelay_SN = 10.        ![Myr] SNs go off at tdelay_SN ... 
@@ -152,21 +150,123 @@ program PhotonsFromStars
      print*,'Not implemented yet...'
      stop
   case('Table')
-     print*,'Not implemented yet...'
-     stop
+     !print*,'Not implemented yet...'
+     !stop
+
+     lambdamin = spec_table_lmin_Ang
+     lambdamax = spec_table_lmax_Ang
+     call ssp_lib_extract_subset(lambdamin,lambdamax,NdotGrid) ! extract the SSP age-met grid of Nphot in lambda range [lambdamin;lambdamax]
+     allocate(Ndot(NdotGrid%nlambda))
+     
+     ! compute the nb of photons per second emitted by each star particle
+     nstars = size(star_age)
+     allocate(sweight(nstars))
+     ! need to store Ndot for each star into NdotStar
+     allocate(NdotStar(nstars,NdotGrid%nlambda))
+     do i = 1,nstars
+        ! interpolate NdotGrid(lambda)
+        call ssp_lib_interpolate(NdotGrid, star_age(i)/1.e3, log10(star_met(i)), Ndot)
+        Ndot = Ndot * star_mass(i) / msun  ! nb of photons / s / A
+        NdotStar(i,:) = Ndot(:)
+        ! integrate nphotPerSecPerMsun(nlambda)
+        sweight(i) = trapz1(NdotGrid%lambda, Ndot, NdotGrid%nlambda)
+     enddo
+     ! compute the total number of photons emitted per second by the sources
+     total_flux = 0.0d0
+     do i=1,nstars
+        total_flux = total_flux + sweight(i)
+     end do
+     if (verbose) write(*,*) '> Total luminosity (nb of photons per second): ',total_flux
+     
+     ! calcul pour chaque particule la luminosite inferieure de son bin dans la distribution cumulative.. 
+     allocate(low_prob(nstars+1))
+     low_prob(1) = 0.0d0
+     do i = 2,nstars
+        low_prob(i) = low_prob(i-1) + sweight(i-1)
+     end do
+     low_prob = low_prob / low_prob(nstars)
+     low_prob(nstars+1) = 1.1  ! higher than upper limit 
+
+     ! for each photon packet, draw the emitting star
+     allocate(x_em(1:3,1:nphot), k_em(1:3,1:nphot), nu_em(1:nphot), nu_star(1:nphot))
+     allocate(low_prob2(NdotGrid%nlambda+1))
+     iseed = ranseed
+     do iphot = 1,nphot
+        r1 = ran3(iseed)
+        ! binary search
+        iup = nstars
+        ilow = 1
+        do while (iup - ilow > 1)
+           imid = (iup+ilow)/2
+           mid  = low_prob(imid)
+           if (r1 >= mid) then 
+              ilow = imid
+           else
+              iup = imid
+           end if
+        end do
+        ! check
+        if (.not. (r1 >= low_prob(ilow) .and. r1 < low_prob(iup) )) then
+           print*,'hi Harley ;) '
+        end if
+        ! photon emitted from star ilow
+        ! give photon the position of the star
+        !print*,iphot,ilow
+        x_em(:,iphot) = star_pos(:,ilow) 
+        ! draw propagation direction
+        call isotropic_direction(k,iseed)
+        k_em(:,iphot) = k
+        ! compute frequency in star frame... here is the difficulty....
+        ! 1/ find the frequency/lambda bin
+        low_prob2(1) = 0.0d0
+        do i = 2,NdotGrid%nlambda
+           low_prob2(i) = low_prob2(i-1) + NdotStar(ilow,i-1)
+        end do
+        low_prob2 = low_prob2 / low_prob2(NdotGrid%nlambda)
+        low_prob2(NdotGrid%nlambda+1) = 1.1  ! higher than upper limit 
+        r2 = ran3(iseed)
+        ! binary search
+        iup2 = NdotGrid%nlambda
+        ilow2 = 1
+        do while (iup2 - ilow2 > 1)
+           imid2 = (iup2+ilow2)/2
+           mid2  = low_prob2(imid2)
+           if (r2 >= mid2) then 
+              ilow2 = imid2
+           else
+              iup2 = imid2
+           end if
+        end do
+        ! check
+        if (.not. (r2 >= low_prob2(ilow2) .and. r2 < low_prob2(iup2) )) then
+           print*,'hi Harley ;) '
+        end if
+        ! photon emitted in the bin ilow2;ilow2+1
+        ! get lamba_em
+        ! TO DO
+        nu_star(iphot) = clight / (NdotGrid%lambda(ilow2)*1e-8) ! Hz
+        ! compute frequency in external frame 
+        scalar = k(1)*star_vel(1,ilow) + k(2)*star_vel(2,ilow) + k(3)*star_vel(3,ilow)
+        nu_em(iphot)  = nu_star(iphot) / (1d0 - scalar/clight)
+     enddo
+     
+
+
   case('Mono')
      
      lambda0 = spec_mono_lambda0
-     call get_Ndot(lambda0, Ndot)  ! charge les SEDs si pas deja fait, et extrait les flux a weight_l0_Ang
+     call ssp_lib_extract_subset(lambda0, lambda0, NdotGrid)  ! charge les SEDs si pas deja fait, et extrait les flux a weight_l0_Ang
      ! -> def le nb phots (at weight_l0_Ang) per sec, per solar mass -> dans un objet class_twod_table (nAge, nZ, 1)
-
+     print*,spec_mono_lambda0, lambda0
+     allocate(Ndot(1))
+     
      ! compute the nb of photons per second emitted by each star particle
      nstars = size(star_age)
      allocate(sweight(nstars))
      do i = 1,nstars
-        print*,i,star_age(i), star_age(i)/1.e3, log10(star_met(i))
-        call twod_table_interpolate(Ndot, star_age(i)/1.e3, log10(star_met(i)), nphotPerSecPerMsun)
-        sweight(i) = nphotPerSecPerMsun(1) * star_mass(i) / msun  ! M_sun
+        !print*,i,star_age(i), star_age(i)/1.e3, log10(star_met(i))
+        call ssp_lib_interpolate(NdotGrid, star_age(i)/1.e3, log10(star_met(i)), Ndot)    ! Ndot number of photons / s / A / Msun
+        sweight(i) = Ndot(1) * star_mass(i) / msun  ! M_sun
         !if (sed_age(iage) < tdelay_SN) then ! SNs go off at 10Myr ... 
         !   sweight(i) = sweight(i)/recyc_frac  !! correct for recycling ... we want the mass of stars formed ...
         !end if
@@ -189,7 +289,7 @@ program PhotonsFromStars
      low_prob = low_prob / low_prob(nstars)
      low_prob(nstars+1) = 1.1  ! higher than upper limit 
 
-     ! tirage
+     ! for each photon packet, draw the emitting star
      allocate(x_em(1:3,1:nphot), k_em(1:3,1:nphot), nu_em(1:nphot), nu_star(1:nphot))
 
      iseed = ranseed
@@ -224,6 +324,7 @@ program PhotonsFromStars
         scalar = k(1)*star_vel(1,ilow) + k(2)*star_vel(2,ilow) + k(3)*star_vel(3,ilow)
         nu_em(iphot)  = nu_star(iphot) / (1d0 - scalar/clight)
      end do
+
   end select
   
 
