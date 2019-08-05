594d84767c92ee8c007760825a679482f8dc5e68
Joki Rosdahl <rosdahl@service0.ice.in2p3.fr>
2019-08-05 13:00:09 +0200

    Dealt with merge conflict

--- f90/module_ramses.f90
+++ f90/module_ramses.f90
@@ -153,7 +153,6 @@ contains
     nleaftot = get_nleaf(repository,snapnum,ncpu_read,cpu_list)
     nvar     = get_nvar(repository,snapnum)
     allocate(ramses_var_all(nvar,nleaftot), xleaf_all(nleaftot,3), leaf_level_all(nleaftot))
-<<<<<<< HEAD
     ! Check whether the ramses output is in single or double precision
     U_precision = nint(get_param_real(repository,snapnum,'U_precision',default_value=8d0))
     print*,'The hydro precision is ',U_precision  !JOKI
@@ -162,9 +161,6 @@ contains
             ,default_value=8d0,rt_info=.true.))
     endif
     print*,'The RT precision is ',RT_precision  !JOKI
-=======
-    ncpu = get_ncpu(repository,snapnum) !!! ncpu should be known before calling read_amr_hydro!!!
->>>>>>> 0a15528fa7f7b7cbdfe8898695eabfc2654b673c
 
     if(verbose) print *,'-- read_leaf_cells_omp: nleaftot(_read), nvar, ncpu(_read) =',nleaftot,nvar,ncpu_read
 
