84411f9dedba03e05d82311f6c355856c736de73
jeremy blaizot <jeremy.blaizot@univ-lyon1.fr>
2019-09-25 15:04:14 +0200

    bug fix in CreateDomDump: arrays were not deallocated before being re-allocated.

--- f90/CreateDomDump.f90
+++ f90/CreateDomDump.f90
@@ -207,8 +207,13 @@ program CreateDomDump
            zmax = decomp_dom_zc(i) + decomp_dom_thickness(i)*0.5d0
            zmin = decomp_dom_zc(i) - decomp_dom_thickness(i)*0.5d0
         end select
+
+        ! clean up arrays before reading again 
+        if (i > 1) deallocate(cpu_list,ramses_var,leaf_level,leaflevel_sel,x_leaf,xleaf_sel,gas_leaves,selected_leaves,ind_sel)
+        print*,xmin,xmax,ymin,ymax,zmin,zmax
         call get_cpu_list_periodic(repository, snapnum, xmin,xmax,ymin,ymax,zmin,zmax, ncpu_read, cpu_list)
         call read_leaf_cells_omp(repository, snapnum, ncpu_read, cpu_list, nleaftot, nvar, x_leaf, ramses_var, leaf_level)
+
         ! Extract and convert properties of cells into gas mix properties
         call gas_from_ramses_leaves(repository,snapnum,nleaftot,nvar,ramses_var, gas_leaves)
         call cpu_time(finish)
@@ -219,6 +224,9 @@ program CreateDomDump
      ! this would be for zoom-in simulations with -Dquadhilbert
      if (reading_method == 'select_onthefly') then
         if (verbose) print*,'Reading leaf cells...'
+
+        ! clean up arrays before reading again 
+        if (i > 1) deallocate(cpu_list,ramses_var,leaf_level,x_leaf,gas_leaves)
         ncpu_read = get_ncpu(repository,snapnum)
         allocate(cpu_list(1:ncpu_read))
         do j=1,ncpu_read
