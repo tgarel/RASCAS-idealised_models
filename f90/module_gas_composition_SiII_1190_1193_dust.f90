514c06452e36ed958c05962c581ba78179840fba
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2021-05-10 16:31:24 +0200

    import peeling off for silicon

--- f90/module_gas_composition_SiII_1190_1193_dust.f90
+++ f90/module_gas_composition_SiII_1190_1193_dust.f90
@@ -46,6 +46,9 @@ module module_gas_composition
   ! public functions:
   public :: gas_from_ramses_leaves,get_gas_velocity,gas_get_scatter_flag,gas_scatter,dump_gas
   public :: read_gas,gas_destructor,read_gas_composition_params,print_gas_composition_params
+  !--PEEL--
+  public :: gas_peeloff_weight,gas_get_tau
+  !--LEEP--
 
 contains
   
@@ -226,6 +229,65 @@ contains
     end select
 
   end subroutine gas_scatter
+
+
+
+  !--PEEL--
+  function  gas_get_tau(cell_gas, distance_cm, nu_cell)
+
+    ! --------------------------------------------------------------------------
+    ! compute total opacity of gas accross distance_cm at freq. nu_cell
+    ! --------------------------------------------------------------------------
+    ! INPUTS:
+    ! - cell_gas : a mix of SiII and dust
+    ! - distance_cm : the distance along which to compute tau [cm]
+    ! - nu_cell : photon frequency in cell's frame [ Hz ]
+    ! OUTPUTS:
+    ! - gas_get_tau : the total optical depth
+    ! --------------------------------------------------------------------------
+
+    ! check whether scattering occurs within cell (scatter_flag > 0) or not (scatter_flag==0)
+    type(gas),intent(in)    :: cell_gas
+    real(kind=8),intent(in) :: distance_cm
+    real(kind=8),intent(in) :: nu_cell
+    real(kind=8)            :: gas_get_tau
+    real(kind=8)            :: tau_SiII_1190, tau_SiII_1193, tau_dust
+
+    ! compute optical depths for different components of the gas.
+    tau_SiII_1190   = get_tau_SiII_1190(cell_gas%nSiII, cell_gas%dopwidth, distance_cm, nu_cell)
+    tau_SiII_1193   = get_tau_SiII_1193(cell_gas%nSiII, cell_gas%dopwidth, distance_cm, nu_cell)
+    tau_dust = get_tau_dust(cell_gas%ndust, distance_cm, nu_cell)
+    gas_get_tau = tau_SiII_1190 + tau_SiII_1193 + tau_dust
+
+    return
+
+  end function gas_get_tau
+
+
+
+  function gas_peeloff_weight(flag,cell_gas,nu_ext,kin,kout,iran)
+
+    integer(kind=4),intent(in)            :: flag
+    type(gas),intent(in)                  :: cell_gas
+    real(kind=8),intent(inout)            :: nu_ext
+    real(kind=8),dimension(3), intent(in) :: kin, kout
+    integer(kind=4),intent(inout)         :: iran
+    real(kind=8)                          :: gas_peeloff_weight
+
+    select case(flag)
+    case(1)
+       gas_peeloff_weight = SiII_1190_peeloff_weight(cell_gas%v, cell_gas%dopwidth, nu_ext, kin, kout, iran)
+    case(2)
+       gas_peeloff_weight = SiII_1193_peeloff_weight(cell_gas%v, cell_gas%dopwidth, nu_ext, kin, kout, iran)
+    case(3)
+       gas_peeloff_weight = dust_peeloff_weight(cell_gas%v, nu_ext, kin, kout)
+    case default
+       print*,'ERROR in module_gas_composition_SiII_1190_1193_dust.f90:gas_peeloff_weight - unknown case : ',flag 
+       stop
+    end select
+
+  end function gas_peeloff_weight
+  !--LEEP--
 
 
 
